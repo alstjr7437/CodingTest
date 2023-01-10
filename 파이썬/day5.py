99267b528a39b2e6f5ec3f5d958c90f433140870
김민석|Minseok Kim <alstjr7437@naver.com>
2023-01-11 00:05:02 +0900

    배열 문제 까지 커밋

--- 파이썬/day5.py
+++ 파이썬/day5.py
@@ -1,4 +1,4 @@
-# 2023-01-01 1차원 배열
+# 2023-01-11 1차원 배열
 
 # 10807 개수 세기
 # a = int(input())
@@ -69,26 +69,28 @@
 
 # 8958 OX 퀴즈
 # score 부분과 sum 부분의 관계 잘 생각하기 (score은 O가 여러번 일 때 2점 더 해지고 그런 부분임 sum은 합계 점수 부분)
-a = int(input())
-for i in range(a):
-    ox = input()
-    score = 0
-    sum = 0
-    for i in range(len(ox)):
-        if ox[i] == 'O':
-            score += 1
-            sum += score
-        else :
-            score = 0
-    print(sum)
+# a = int(input())
+# for i in range(a):
+#     ox = input()
+#     score = 0
+#     sum = 0
+#     for i in range(len(ox)):
+#         if ox[i] == 'O':
+#             score += 1
+#             sum += score
+#         else :
+#             score = 0
+#     print(sum)
 
 # 4344 평균은 넘겠지
-a = int(input())
-for i in range(a):
-    score = list(map(int,input().split()))
-    sum = 0
-    print(len(score))
-    for j in range(1, len(score)+1):
-        print('hello')
-    avg = sum / score[i]
-    print(avg)
+# .3f와 round의 차이점 .000까지 나오는 부분이 다름, sum(score[1:])는 score 2번째 부터 쭉 더하는거
+# a = int(input())
+# for i in range(a):
+#     score = list(map(int,input().split()))
+#     avg = sum(score[1:]) / score[0]
+#     count = 0
+#     for j in range(1, score[0]+1):
+#         if avg < score[j]:
+#             count += 1
+#     b = count/score[0] * 100
+#     print('%.3f'%b,end='%\n')
