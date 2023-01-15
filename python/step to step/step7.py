66b47c8aa5eab0877bc62d4a46c704f34ec5d452
김민석|Minseok Kim <alstjr7437@naver.com>
2023-01-15 21:58:32 +0900

    기본 수학 1문제 커밋

--- python/step to step/step7.py
+++ python/step to step/step7.py
@@ -1,8 +1,18 @@
-# 2023-01-14 기본 수학 1단계
+# 2023-01-14 ~ (부경대 면접으로 인해 한문제씩)
+# 기본 수학 1단계
 
 # 1712 손익 분기점
-a,b,c = map(int,input().split())
-if b>=c:
-    print(-1)
-else :
-    print(a//(c-b)+1)
+# a,b,c = map(int,input().split())
+# if b>=c:
+#     print(-1)
+# else :
+#     print(a//(c-b)+1)
+
+# 2292 벌집
+# a = int(input())
+# home = 1
+# count = 1
+# while a > home :
+#     home += count * 6
+#     count += 1
+# print(count)
